ee2323d3de5d364c052dd45d165ddf6795a28bdb
Avon46 <142923631+Avon46@users.noreply.github.com>
2026-05-29 11:12:29 +0800

    add api front

--- src/main/java/com/example/demo/controller/PolicyController.java
+++ src/main/java/com/example/demo/controller/PolicyController.java
@@ -15,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/api/policies")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class PolicyController {
 
     private final PolicyService policyService;
